58e474681f3363ed782be5dddb82320f81f20b74
Gustavo Sales <gustavo@gsales.io>
2023-08-27 23:30:24 -0300

    fix: header position

--- apps/mashgin-web-app/libs/components/header.tsx
+++ apps/mashgin-web-app/libs/components/header.tsx
@@ -8,12 +8,14 @@ const GoToChartButton = dynamic(
 
 export default function Header() {
   return (
-    <header className="container flex items-center justify-between fixed bg-zinc-50 top-0 z-10 py-3 border-b border-solid border-zinc-200">
-      <div className="text-2xl font-bold text-zinc-900 cursor-default flex items-center gap-1">
-        <AiFillStar className="-mt-1 fill-yellow-600" /> Mashgin Restaurant
-      </div>
+    <header className="w-full fixed bg-zinc-50 top-0 z-10 py-3 border-b border-solid border-zinc-200">
+      <div className="container flex items-center justify-between ">
+        <div className="text-2xl font-bold text-zinc-900 cursor-default flex items-center gap-1">
+          <AiFillStar className="-mt-1 fill-yellow-600" /> Mashgin Restaurant
+        </div>
 
-      <GoToChartButton />
+        <GoToChartButton />
+      </div>
     </header>
   );
 }
